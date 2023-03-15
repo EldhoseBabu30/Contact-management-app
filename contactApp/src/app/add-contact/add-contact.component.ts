0e11efa3648337b46b4f01432772b4d0e5daee40
Eldhose Babu <eldhosebabu316@gmail.com>
2023-03-16 02:06:38 +0530

    Contact-management-app

---
+++ contactApp/src/app/add-contact/add-contact.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MyContact } from 'src/models/myContact';
+import { ApiService } from '../services/api.service';
+
+@Component({
+  selector: 'app-add-contact',
+  templateUrl: './add-contact.component.html',
+  styleUrls: ['./add-contact.component.css']
+})
+export class AddContactComponent implements OnInit {
+
+  allGroups:any[]=[]
+  contactName:string=''
+  contact:MyContact = {} as MyContact
+  constructor(private api:ApiService,private router:Router){}
+  
+  ngOnInit():void{
+    this.api.getAllGroups()
+    .subscribe((data:any)=>{
+      this.allGroups = data
+
+    })
+
+  }
+  addContact(){
+    this.api.addContact(this.contact)
+    .subscribe(
+      (data:any)=>{
+        //navigate to admin page
+        this.router.navigateByUrl('')
+
+      }
+    )
+
+  }
+
+}
---
+++ contactApp/src/app/services/api.service.ts
@@ -0,0 +1,62 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { MyContact } from 'src/models/myContact';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  baseUrl:string ='http://localhost:3000/contacts'
+  
+  constructor(private http:HttpClient) { }
+
+  //function for get allcontacts
+
+  getAllContacts():Observable<MyContact>{
+   return this.http.get(this.baseUrl)
+
+
+  }
+
+  //function for view a particular contact
+
+  viewContact(contactId:string) {
+   return this.http.get(`${this.baseUrl}/${contactId}`)
+    
+  }
+
+  //function for getting an particular group
+
+  getGroupName(groupId:string){
+    return this.http.get('http://localhost:3000/groups/'+groupId)
+
+  }
+
+  //function for fetch all groups from http://localhost:3000/groups
+  getAllGroups(){
+    return this.http.get('http://localhost:3000/groups')
+  }
+
+  //function for adding new contact to server
+  addContact(contactBody:any){
+    return this.http.post(this.baseUrl,contactBody)
+
+  }
+
+  //fumction for deleting a contact
+  deleteContact(contactId:any){
+  return this.http.delete(`${this.baseUrl}/${contactId}`)
+  }
+
+  //update contact details according to user select
+
+  updateContact(contactId:any,contactBody:any){
+    return this.http.put(`${this.baseUrl}/${contactId}`,contactBody)
+  }
+
+
+
+
+}
